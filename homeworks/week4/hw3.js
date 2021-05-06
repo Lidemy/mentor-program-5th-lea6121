62c92cb711e837cb0a3b18f449b0f33fc9e97ece
lea6121 <shuyun4506@gmail.com>
2021-05-06 11:46:41 +0800

    finished hw4, hw3 modified

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -27,24 +27,3 @@ function listCountryData() {
 }
 
 listCountryData()
-
-/* 範例
-
-node hw3.js tai
-
-============
-國家：Taiwan
-首都：Taipei
-貨幣：TWD
-國碼：886
-============
-國家：United Kingdom of Great Britain and Northern Ireland
-首都：London
-貨幣：GBP
-國碼：44
-============
-國家：Lao People's Democratic Republic
-首都：Vientiane
-貨幣：LAK
-國碼：856
-*/
